215b9009f1f5639fc3e46c12cf4544e1b7502056
Patrick Meenan <pmeenan@chromium.org>
2018-05-22 07:04:28 -0700

    Initial work on jni forwarder

---
+++ app/src/main/cpp/forwarder.h
@@ -0,0 +1,8 @@
+//
+// Created by pmeenan on 5/21/18.
+//
+
+#ifndef GNIREHTET_FORWARDER_H
+#define GNIREHTET_FORWARDER_H
+
+#endif //GNIREHTET_FORWARDER_H
---
+++ app/src/main/java/com/genymobile/gnirehtet/forwarder.c
@@ -0,0 +1,5 @@
+//
+// Created by pmeenan on 5/21/18.
+//
+
+#include "forwarder.h"
